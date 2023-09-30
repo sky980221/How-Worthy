31dce208b3a826fec1caef45e66466c9a0db2739
Dori <sky980221@naver.com>
2023-09-30 18:33:46 +0900

    feat: spring 세팅

---
+++ src/test/java/howWorthy/howWorthy/HowWorthyApplicationTests.java
@@ -0,0 +1,13 @@
+package howWorthy.howWorthy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowWorthyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
